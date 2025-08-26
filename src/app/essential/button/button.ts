835cd535886e8086379ccd3090ce38a09d4db263
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-08-26 12:25:28 +0800

    text input works

--- src/app/essential/button/button.ts
+++ src/app/essential/button/button.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './button.css'
 })
 export class Button {
-
+  readonly text= input<string>('Button Name');
 }
