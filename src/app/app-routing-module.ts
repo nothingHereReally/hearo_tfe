df0c3b72fea751deaf614d5c6866b5d67798abe2
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-08-26 12:21:23 +0800

    commented routing button

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 
-import { Button } from './essential/button/button';
+// import { Button } from './essential/button/button';
 
 const routes: Routes = [
-  {path: 'button', component: Button},
+  // {path: 'button', component: Button},
 ];
 
 @NgModule({
