7c431e884591b3b2b7c0b68bb4ab736230221965
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-08-26 12:40:18 +0800

    added input vars for later use

--- src/app/essential/button/button.ts
+++ src/app/essential/button/button.ts
@@ -8,4 +8,7 @@ import { Component, input } from '@angular/core';
 })
 export class Button {
   readonly text= input<string>('Button Name');
+  readonly styleButton= input<string>('solid'); /* solid, outline, naked, spacial, link,
+  danger_solid, danger_outline, danger_naked */
+  readonly iconDir= input<string>('blah');
 }
