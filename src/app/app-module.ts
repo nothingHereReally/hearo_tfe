b780c4608f5af3ee7f486d5135b8934a3e8bff46
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-08-27 12:57:46 +0800

    added FormsModule for 2way-binding

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,5 +1,6 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
@@ -14,7 +15,8 @@ import { Input } from './essential/input/input';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
