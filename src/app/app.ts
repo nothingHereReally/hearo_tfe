f68de0651cd74725b449457789cbeff21fa8f3be
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-08-27 14:25:14 +0800

    removed due to not used anymore

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +7,4 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title= signal('hearo_tfe');
-  protected inputText= signal('some thing blah blah');
-
-
-  public onClick(){
-    console.log(`from app.ts__${Math.random()} input ${this.inputText()}`)
-  }
 }
