63dabf302c5cc939808df2b5f971d3be8be936a7
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-08-28 15:27:26 +0800

    moved code to DataTable component, still creating component( not yet finished )

--- src/app/page/home/home.ts
+++ src/app/page/home/home.ts
@@ -7,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './home.css'
 })
 export class Home {
-
+  protected caption: string= 'Hospital Head Request';
+  protected titles: Array<string>= ['Name', 'Hospital', 'Date and Time', 'Review'];
+  protected contents: Array< Array<string> >= [
+    ['Liza Mae Villanueva', 'Cebu City Medical Center', 'July 4, 2025 8:50am', 'https://example.com/'],
+    ['Jessica Bautista', 'Camp Lapu-Lapu Station Hospital', 'July 6, 2025 8:30am', 'https://example.com/'],
+    ['Maria Clara Santos', 'Adventist Hospital - Cebu, Inc.', 'July 9, 2025 9:55am', 'https://example.com/'],
+    ['Lisa Garcia', 'Barili District Hospital', 'July 10, 2025 9:10am', 'https://example.com/'],
+    ['Anna Cruz', 'Cebu South Medical Center', 'July 11, 2025 9:50am', 'https://example.com/']
+  ]
 }
