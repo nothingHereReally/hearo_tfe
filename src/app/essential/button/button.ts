362f923ccc57813becbe075f0e258203f1ea945d
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-08-28 18:03:15 +0800

    added simi-colon

--- src/app/essential/button/button.ts
+++ src/app/essential/button/button.ts
@@ -45,61 +45,61 @@ export class Button implements OnInit{
           this.iconfile_pressed= `/icon_sc_900/${this.iconname()}.svg`;
         }else if( this.styleButton()===validStyles[1] ){ /* style-outline */
           this.iconfiledir.set(`/icon_pc1_300/${this.iconname()}.svg`);
-          this.iconfile_init= `/icon_pc1_300/${this.iconname()}.svg`
-          this.iconfile_hover= `/icon_pc1_300/${this.iconname()}.svg`
-          this.iconfile_pressed= `/icon_sc_900/${this.iconname()}.svg`
+          this.iconfile_init= `/icon_pc1_300/${this.iconname()}.svg`;
+          this.iconfile_hover= `/icon_pc1_300/${this.iconname()}.svg`;
+          this.iconfile_pressed= `/icon_sc_900/${this.iconname()}.svg`;
         }else if( this.styleButton()===validStyles[2] ){ /* style-naked */
-          this.iconfiledir.set(`/icon_pc1_300/${this.iconname()}.svg`)
-          this.iconfile_init= `/icon_pc1_300/${this.iconname()}.svg`
-          this.iconfile_hover= `/icon_pc1_300/${this.iconname()}.svg`
-          this.iconfile_pressed= `/icon_pc1_300/${this.iconname()}.svg`
+          this.iconfiledir.set(`/icon_pc1_300/${this.iconname()}.svg`);
+          this.iconfile_init= `/icon_pc1_300/${this.iconname()}.svg`;
+          this.iconfile_hover= `/icon_pc1_300/${this.iconname()}.svg`;
+          this.iconfile_pressed= `/icon_pc1_300/${this.iconname()}.svg`;
         }else if( this.styleButton()===validStyles[4] ){ /* style-link */
-          this.iconfiledir.set(`/icon_tc2_400/link.svg`)
-          this.iconfile_init= `/icon_tc2_400/link.svg`
-          this.iconfile_hover= `/icon_tc2_400/link.svg`
-          this.iconfile_pressed= `/icon_sc_900/link.svg`
+          this.iconfiledir.set(`/icon_tc2_400/link.svg`);
+          this.iconfile_init= `/icon_tc2_400/link.svg`;
+          this.iconfile_hover= `/icon_tc2_400/link.svg`;
+          this.iconfile_pressed= `/icon_sc_900/link.svg`;
         }else if( this.styleButton()===validStyles[5] ){ /* style-danger-solid */
-          this.iconfiledir.set(`/icon_sc_900/${this.iconname()}.svg`)
-          this.iconfile_init= `/icon_sc_900/${this.iconname()}.svg`
-          this.iconfile_hover= `/icon_tc3_500/${this.iconname()}.svg`
-          this.iconfile_pressed= `/icon_sc_900/${this.iconname()}.svg`
+          this.iconfiledir.set(`/icon_sc_900/${this.iconname()}.svg`);
+          this.iconfile_init= `/icon_sc_900/${this.iconname()}.svg`;
+          this.iconfile_hover= `/icon_tc3_500/${this.iconname()}.svg`;
+          this.iconfile_pressed= `/icon_sc_900/${this.iconname()}.svg`;
         }else if( this.styleButton()===validStyles[6] ){ /* style-danger-outline */
-          this.iconfiledir.set(`/icon_tc3_500/${this.iconname()}.svg`)
-          this.iconfile_init= `/icon_tc3_500/${this.iconname()}.svg`
-          this.iconfile_hover= `/icon_tc3_500/${this.iconname()}.svg`
-          this.iconfile_pressed= `/icon_sc_900/${this.iconname()}.svg`
+          this.iconfiledir.set(`/icon_tc3_500/${this.iconname()}.svg`);
+          this.iconfile_init= `/icon_tc3_500/${this.iconname()}.svg`;
+          this.iconfile_hover= `/icon_tc3_500/${this.iconname()}.svg`;
+          this.iconfile_pressed= `/icon_sc_900/${this.iconname()}.svg`;
         }else if( this.styleButton()===validStyles[7] ){ /* style-danger-naked */
-          this.iconfiledir.set(`/icon_tc3_500/${this.iconname()}.svg`)
-          this.iconfile_init= `/icon_tc3_500/${this.iconname()}.svg`
-          this.iconfile_hover= `/icon_tc3_500/${this.iconname()}.svg`
-          this.iconfile_pressed= `/icon_sc_900/${this.iconname()}.svg`
+          this.iconfiledir.set(`/icon_tc3_500/${this.iconname()}.svg`);
+          this.iconfile_init= `/icon_tc3_500/${this.iconname()}.svg`;
+          this.iconfile_hover= `/icon_tc3_500/${this.iconname()}.svg`;
+          this.iconfile_pressed= `/icon_sc_900/${this.iconname()}.svg`;
         }
 
 
       }else if( this.styleButton()===validStyles[4] ){ /* style-link */
-          this.iconfiledir.set(`/icon_tc2_400/link.svg`)
-          this.iconfile_init= `/icon_tc2_400/link.svg`
-          this.iconfile_hover= `/icon_tc2_400/link.svg`
-          this.iconfile_pressed= `/icon_sc_900/link.svg`
+          this.iconfiledir.set(`/icon_tc2_400/link.svg`);
+          this.iconfile_init= `/icon_tc2_400/link.svg`;
+          this.iconfile_hover= `/icon_tc2_400/link.svg`;
+          this.iconfile_pressed= `/icon_sc_900/link.svg`;
       }
     }else{
-      throw new TypeError(`validstyles can only be 1 of the following: ${validStyles}`)
+      throw new TypeError(`validstyles can only be 1 of the following: ${validStyles}`);
     }
   }
 
 
   public onHover(){ /* 1) onHover; 3) onHover */
-    this.iconfiledir.set(this.iconfile_hover)
+    this.iconfiledir.set(this.iconfile_hover);
   }
   public onPressing(){ /* 2) onPressing */
-    this.iconfiledir.set(this.iconfile_pressed)
+    this.iconfiledir.set(this.iconfile_pressed);
   }
   public onClick(){ /* 4) onClick */
-    this.iconfiledir.set(this.iconfile_pressed)
-    this.onHover()
+    this.iconfiledir.set(this.iconfile_pressed);
+    this.onHover();
     this.outOnClick.emit();
   }
   public initIcon(){ /* 5) initIcon, leaving hover( outside button ) */
-    this.iconfiledir.set(this.iconfile_init)
+    this.iconfiledir.set(this.iconfile_init);
   }
 }
