98c1fd0d2a3e2842a13f17475020f11964fbaf1c
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-09-01 16:34:40 +0800

    added initial part for verify-to-register page

---
+++ src/app/page/verify-to-register/verify-to-register.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verify-to-register',
+  standalone: false,
+  templateUrl: './verify-to-register.html',
+  styleUrl: './verify-to-register.css'
+})
+export class VerifyToRegister {
+
+}
