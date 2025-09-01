4b62d493cbfbf8962c713c7ee88121c1d081ad36
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-09-02 00:27:38 +0800

    changed to native play

--- src/app/page/verify-to-register/verify-to-register.ts
+++ src/app/page/verify-to-register/verify-to-register.ts
@@ -20,6 +20,16 @@ export class VerifyToRegister implements AfterViewInit {
       audio: false
     }).then(stream => {
       this.videoRef().nativeElement.srcObject= stream;
+      this.videoRef().nativeElement.onloadedmetadata= ()=>{
+          this.videoRef().nativeElement.play();
+      }
+      setTimeout(()=>{
+        console.log(this.videoRef().nativeElement);
+        console.log(this.videoRef().nativeElement.videoWidth);
+        console.log(this.videoRef().nativeElement.videoHeight);
+        console.log(this.videoRef().nativeElement.width);
+        console.log(this.videoRef().nativeElement.height);
+      }, 3000);
     }).catch(error => {
       console.error('Error accessing media devices.', error);
     });
