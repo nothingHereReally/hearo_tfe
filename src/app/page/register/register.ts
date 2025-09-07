b6442c13e5e4eaa82453d6c03fdb183184a04c12
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-09-07 16:57:37 +0800

    finally structured registration UI for hearo team

--- src/app/page/register/register.ts
+++ src/app/page/register/register.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -6,6 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
-export class Register {
+export class Register implements OnInit{
+  protected warning_firstname: WritableSignal<string>= signal('warning first name');
+  protected warning_lastname: WritableSignal<string>= signal('warning last name');
+  protected warning_email: WritableSignal<string>= signal('warning email');
+  protected warning_password: WritableSignal<string>= signal('warning password');
+  protected warning_rpassword: WritableSignal<string>= signal('warning retype password');
 
+  ngOnInit(): void {
+      console.log("hello world: ", Math.random());
+  }
 }
