856a0732fee9b17df559f78a8a66884f2831b8eb
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-09-08 12:10:00 +0800

    added function to create user account via http client

--- src/app/api-service/auth-user.ts
+++ src/app/api-service/auth-user.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { API_DOMAIN } from '../model/constant';
 import { CookieService } from 'ngx-cookie-service';
 import { Token } from '../model/token';
+import { RegisterUser } from '../model/register-user';
 
 @Injectable({
   providedIn: 'root'
@@ -40,6 +41,38 @@ export class AuthUser {
         observe: 'body'
       }
     )
+  }
+
+
+  public createAccountHearoAccount(hearoUser: RegisterUser): Observable<any>{
+    let token: Token= this.getToken_AccessQRAccount()==null? {
+      access: '',
+      refresh: ''
+    }: this.getToken_AccessQRAccount()!;
+    if( token.access=='' ){
+      throw new TypeError("Access token for QR access account can't be empty");
+    }
+    let header: HttpHeaders= new HttpHeaders({
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+      "Authorization": `Bearer ${token.access}`
+    })
+    let newHearoUser= {
+      "account_type": "ht",
+      "email": hearoUser.email,
+      "username": hearoUser.username,
+      "password": hearoUser.password,
+      "first_name": hearoUser.first_name,
+      "last_name": hearoUser.last_name
+    };
+    return this.http.post<any>(
+      API_DOMAIN+"api/v1/users/",
+      newHearoUser,{
+        headers: header,
+        observe: 'body',
+        credentials: 'include'
+      },
+    );
   }
 
 
