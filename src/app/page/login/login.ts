b55e185b659710ae82f68981adee165bad69c43c
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-09-08 20:00:55 +0800

    added logic to save token on cookie when logging in

--- src/app/page/login/login.ts
+++ src/app/page/login/login.ts
@@ -1,5 +1,10 @@
-import { Component, signal, WritableSignal } from '@angular/core';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
+
+
 import { LoginField } from '../../model/login-field';
+import { TIME_ERROR_DISPLAY } from '../../model/constant';
+import { AuthUser } from '../../api-service/auth-user';
+import { Token } from '../../model/token';
 
 @Component({
   selector: 'app-login',
@@ -8,6 +13,7 @@ import { LoginField } from '../../model/login-field';
   styleUrl: './login.css'
 })
 export class Login {
+  private authUser: AuthUser= inject(AuthUser);
   protected hearoUser: WritableSignal<LoginField>= signal({
     username: '',
     password: ''
@@ -19,6 +25,32 @@ export class Login {
 
 
   protected loggingIn(): void{
-    console.log("user: ", this.hearoUser());
+    if( this.hearoUser().username!='' && this.hearoUser().password!='' ){
+      this.authUser.userLogin(this.hearoUser()).subscribe({
+        next: (r: Token)=>{
+          this.authUser.saveAccountToken(r);
+        },
+        error: (err: any)=>{
+          this.warnings.update(value=>{ value.password="Incorrect Username or Password"; return value });
+          setTimeout(()=>{
+            this.warnings.update(value=>{ value.password=""; return value });
+          }, TIME_ERROR_DISPLAY);
+        },
+        complete: ()=>{
+        }
+      });
+    }
+    if( this.hearoUser().username=='' ){
+      this.warnings.update(value=>{ value.username="Please fill your Username"; return value });
+      setTimeout(()=>{
+        this.warnings.update(value=>{ value.username=""; return value });
+      }, TIME_ERROR_DISPLAY);
+    }
+    if( this.hearoUser().password=='' ){
+      this.warnings.update(value=>{ value.password="Please fill your Password"; return value });
+      setTimeout(()=>{
+        this.warnings.update(value=>{ value.password=""; return value });
+      }, TIME_ERROR_DISPLAY);
+    }
   }
 }
