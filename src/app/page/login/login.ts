0fcaa603f66c143d2e3689a5325ef45e33be36a8
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-09-08 22:40:58 +0800

    added to save current user logged token as cookie for persistence

--- src/app/page/login/login.ts
+++ src/app/page/login/login.ts
@@ -1,10 +1,12 @@
-import { Component, inject, signal, WritableSignal } from '@angular/core';
+import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
 
 
 import { LoginField } from '../../model/login-field';
 import { TIME_ERROR_DISPLAY } from '../../model/constant';
 import { AuthUser } from '../../api-service/auth-user';
 import { Token } from '../../model/token';
+import { Router } from '@angular/router';
+import { RegisterUser } from '../../model/register-user';
 
 @Component({
   selector: 'app-login',
@@ -12,8 +14,11 @@ import { Token } from '../../model/token';
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
-export class Login {
+export class Login implements OnInit{
   private authUser: AuthUser= inject(AuthUser);
+  private router: Router= inject(Router);
+
+
   protected hearoUser: WritableSignal<LoginField>= signal({
     username: '',
     password: ''
@@ -24,11 +29,39 @@ export class Login {
   });
 
 
+  ngOnInit(): void {
+    let authToken: Token|null= this.authUser.getAccountToken();
+    if( authToken!=null ){
+      this.authUser.verifyToken(authToken.access).subscribe({
+        next: (r: any)=>{
+          this.router.navigate(['/home']);
+        },
+        error: (err: any)=>{
+          this.authUser.getTokenViaRefresh(authToken.refresh).subscribe({
+            next: (validToken: Token)=>{
+              this.authUser.saveAccountToken(validToken);
+              this.router.navigate(['/home']);
+            },
+            error: (err: any)=>{
+              /* refresh token already expired, so need to fill login form again */
+            },
+            complete: ()=>{
+            }
+          });
+        },
+        complete: ()=>{
+        }
+      });
+    }
+  }
+
+
   protected loggingIn(): void{
     if( this.hearoUser().username!='' && this.hearoUser().password!='' ){
       this.authUser.userLogin(this.hearoUser()).subscribe({
         next: (r: Token)=>{
           this.authUser.saveAccountToken(r);
+          this.router.navigate(['/home']);
         },
         error: (err: any)=>{
           this.warnings.update(value=>{ value.password="Incorrect Username or Password"; return value });
