81456f1b95a28ccdcf31cde43987e2452387943c
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-03-14 12:37:02 +0800

    separated headers for it to be reusable

---
+++ src/app/model/tools.ts
@@ -0,0 +1,7 @@
+import { HttpHeaders } from '@angular/common/http';
+
+
+export const headers_sendreceive_json: HttpHeaders= new HttpHeaders({
+  "Content-Type": "application/json",
+  "Accept": "application/json",
+})
