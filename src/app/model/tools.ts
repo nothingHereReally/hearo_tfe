9365b62ba24f6f5244b445fbdc0aee88ab4b34e8
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-03-14 13:03:14 +0800

    added 'Request' to be more explicit/clear on its job

--- src/app/model/tools.ts
+++ src/app/model/tools.ts
@@ -1,10 +1,10 @@
 import { HttpHeaders } from '@angular/common/http';
 
 
-export const httpHeadersSendReceiveJson: HttpHeaders= new HttpHeaders({
+export const httpRequestHeadersSendReceiveJson: HttpHeaders= new HttpHeaders({
   "Content-Type": "application/json",
   "Accept": "application/json",
 })
-export const httpHeadersReceiveJson: HttpHeaders= new HttpHeaders({
+export const httpRequestHeadersReceiveJson: HttpHeaders= new HttpHeaders({
    "Accept": "application/json"
 });
