2c9ae8716eca145799ca507d5b0701189bf8c07a
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-03-14 12:48:24 +0800

    added headersReceiveJson for http request later

--- src/app/model/tools.ts
+++ src/app/model/tools.ts
@@ -1,7 +1,10 @@
 import { HttpHeaders } from '@angular/common/http';
 
 
-export const headers_sendreceive_json: HttpHeaders= new HttpHeaders({
+export const headersSendReceiveJson: HttpHeaders= new HttpHeaders({
   "Content-Type": "application/json",
   "Accept": "application/json",
 })
+export const headersReceiveJson: HttpHeaders= new HttpHeaders({
+   "Accept": "application/json"
+});
