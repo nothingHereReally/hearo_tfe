6d40636423f23b168a32821cc1bcef811638f696
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-03-16 23:19:53 +0800

    created goTo_verify_to_register_pageIfNotValidQRToken for verify-to-register redirection

--- src/app/api-service/auth-user.ts
+++ src/app/api-service/auth-user.ts
@@ -230,4 +230,24 @@ export class AuthUser {
     await this.router.navigate(['/login']);
     return true;
   }
+  public async goTo_verify_to_register_pageIfNotValidQRToken(): Promise<boolean>{
+    let authToken: Token|null= this.getToken_AccessQRAccount();
+    if( authToken!=null &&
+        authToken.access!='' && authToken.access!=null &&
+        authToken.refresh!='' && authToken.refresh!=null ){
+      if(await this.isTokenValid(authToken.access)){
+        return false;
+      }else if(await this.isTokenValid(authToken.refresh)){
+        try{
+          authToken= await firstValueFrom(this.getTokenViaRefreshHttpPost(authToken.refresh));
+          if( authToken!=null ){
+            this.saveToken_AccessQRAccount(authToken)
+          }
+          return false; /* due2not going to /verify-to-register */
+        }catch(err){ /* just continue below for DRY */ }
+      }
+    }
+    await this.router.navigate(['/verify-to-register']);
+    return true;
+  }
 }
