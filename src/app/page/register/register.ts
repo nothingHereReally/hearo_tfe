0b79d623efbd166e1afe437ad9b6c3a4c28273b9
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-03-17 12:52:11 +0800

    changed to use goTo_verify_to_register_pageIfNotValidQRToken from AuthUser

--- src/app/page/register/register.ts
+++ src/app/page/register/register.ts
@@ -43,38 +43,11 @@ export class Register implements OnInit, OnDestroy{
   private subscription: Array<Subscription>= [];
 
   ngOnInit(): void {
-    this.authTokenQR= this.authUser.getToken_AccessQRAccount()==null? this.authTokenQR: this.authUser.getToken_AccessQRAccount()!;
     this.__checkToken();
   }
-  private __checkToken(): void{
-    if( this.authTokenQR.access=='' || this.authTokenQR.refresh=='' ){
-      this.router.navigate(['/verify-to-register'])
-    }
-    this.subscription.push(this.authUser.createHearoAccountHttpPost(this.hearoUser()).subscribe({
-      next: (r: any)=>{
-      },
-      error: (err: any)=>{
-        if( ( err.error.email==null && err.error.email==undefined ) &&
-          ( err.error.username==null && err.error.username==undefined ) &&
-          ( err.error.password==null && err.error.password==undefined ) &&
-          ( err.error.first_name==null && err.error.first_name==undefined ) &&
-          ( err.error.last_name==null && err.error.last_name==undefined )
-        ){
-          this.subscription.push(this.authUser.getTokenViaRefreshHttpPost(this.authTokenQR.refresh).subscribe({
-            next: (r: any|Token)=>{
-              this.authTokenQR.access= r.access;
-              this.authTokenQR.refresh= r.refresh;
-              this.authUser.saveToken_AccessQRAccount(this.authTokenQR);
-            },
-            error: (err: any)=>{
-              this.router.navigate(['/verify-to-register']);
-            }
-          }));
-        }
-      },
-      complete: ()=>{
-      }
-    }))
+  private async __checkToken(): Promise<void>{
+    await this.authUser.goTo_verify_to_register_pageIfNotValidQRToken();
+    this.authTokenQR= this.authUser.getToken_AccessQRAccount()==null? this.authTokenQR: this.authUser.getToken_AccessQRAccount()!;
   }
 
   ngOnDestroy(): void {
