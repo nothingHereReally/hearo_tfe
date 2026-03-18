fe23d9e625389fea59ffc3b7e5e4c1e9218c1e42
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-03-18 23:12:03 +0800

    correct to include all validation from api

--- src/app/page/register/register.ts
+++ src/app/page/register/register.ts
@@ -2,10 +2,9 @@ import { Component, inject, OnInit, signal, WritableSignal, OnDestroy } from '@a
 import { environment as env } from '../../../environment/environment';
 import { Token } from '../../model/token';
 import { AuthUser } from '../../api-service/auth-user';
-import { RegisterUser } from '../../model/register-user';
+import { RegisterUser, RegisterUserWarnings } from '../../model/register-user';
 import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
-import { CreateAccountError } from '../../model/create-account-error';
+import { firstValueFrom, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-register',
@@ -14,13 +13,13 @@ import { CreateAccountError } from '../../model/create-account-error';
   styleUrl: './register.css'
 })
 export class Register implements OnInit, OnDestroy{
-  protected warnings: WritableSignal<RegisterUser>= signal({
-    first_name: '',
-    last_name: '',
-    email: '',
-    username: '',
-    password: '',
-    retype_password: ''
+  protected warnings: WritableSignal<RegisterUserWarnings>= signal({
+    first_name: [],
+    last_name: [],
+    email: [],
+    username: [],
+    password: [],
+    retype_password: []
   });
   protected hearoUser: WritableSignal<RegisterUser>= signal({
     first_name: '',
@@ -46,8 +45,9 @@ export class Register implements OnInit, OnDestroy{
     this.__checkTokenAsync();
   }
   private async __checkTokenAsync(): Promise<void>{
-    await this.authUser.goTo_verify_to_register_pageIfNotValidQRTokenAsync();
-    this.authTokenQR= this.authUser.getToken_AccessQRAccount()==null? this.authTokenQR: this.authUser.getToken_AccessQRAccount()!;
+    if(await this.authUser.goTo_verify_to_register_pageIfNotValidQRTokenAsync()==false){
+      this.authTokenQR= this.authUser.getToken_AccessQRAccount()!;
+    }
   }
 
   ngOnDestroy(): void {
@@ -60,123 +60,146 @@ export class Register implements OnInit, OnDestroy{
     this.hearoUser().email!='' && this.hearoUser().password!='' &&
     this.hearoUser().password==this.hearoUser().retype_password;
   }
-
-
-  protected createAccount(): void{
-    if( this.__checkIfAllFilled() ){
-
-
-      this.subscription.push(this.authUser.createHearoAccountHttpPost(this.hearoUser()).subscribe({
-        next: (r: any)=>{
-          if(r.message=="Hearo user successfully created"){
-            this.warnings.update(value=>{value.retype_password="Created Account Successfully ✔"; return value})
-            setTimeout(()=>{
-              this.router.navigate(['/login']);
-            }, env.TIME_ERROR_DISPLAY/2);
-          }
-        },
-        error: (err: any)=>{
-          let msgErrors: CreateAccountError= {
-            first_name: err.error.first_name,
-            last_name: err.error.last_name,
-            email: err.error.email,
-            username: err.error.username,
-            password: err.error.password,
-          };
-          if( msgErrors.email==undefined &&
-            msgErrors.username==undefined &&
-            msgErrors.password==undefined &&
-            msgErrors.first_name==undefined &&
-            msgErrors.last_name==undefined
-          ){
-            this.subscription.push(this.authUser.getTokenViaRefreshHttpPost(this.authTokenQR.refresh).subscribe({
-              next: (r: any|Token)=>{
-                this.authTokenQR.access= r.access;
-                this.authTokenQR.refresh= r.refresh;
-                this.authUser.saveToken_AccessQRAccount(this.authTokenQR);
-                this.createAccount();
-              },
-              error: (err: any)=>{
-                this.router.navigate(['/verify-to-register']);
-              }
-            }));
-          }
-
-
-          if( msgErrors.first_name!=undefined ){
-            this.warnings.update(value=>{ value.first_name=String(msgErrors.first_name![0]); return value });
-            setTimeout(()=>{
-              this.warnings.update(value=>{ value.first_name=''; return value });
-            }, env.TIME_ERROR_DISPLAY);
-          }
-          if( msgErrors.last_name!=undefined ){
-            this.warnings.update(value=>{ value.last_name=String(msgErrors.last_name![0]); return value });
-            setTimeout(()=>{
-              this.warnings.update(value=>{ value.last_name=''; return value });
-            }, env.TIME_ERROR_DISPLAY);
-          }
-          if( msgErrors.email!=undefined ){
-            this.warnings.update(value=>{ value.email=String(msgErrors.email![0]); return value });
-            setTimeout(()=>{
-              this.warnings.update(value=>{ value.email=''; return value })
-            }, env.TIME_ERROR_DISPLAY);
-          }
-          if( msgErrors.username!=undefined ){
-            this.warnings.update(value=>{ value.username=String(msgErrors.username![0]); return value });
-            setTimeout(()=>{
-              this.warnings.update(value=>{ value.username=''; return value });
-            }, env.TIME_ERROR_DISPLAY);
-          }
-          if( msgErrors.password!=undefined ){
-            this.warnings.update(value=>{ value.password=String(msgErrors.password![0]); return value });
-            setTimeout(()=>{
-              this.warnings.update(value=>{ value.password=''; return value });
-            }, env.TIME_ERROR_DISPLAY);
-          }
+  private __writeWarnings2fillUp(): void{
+    if( this.hearoUser().first_name=='' ){
+      this.warnings.update(
+        value=>{
+          value.first_name= [...value.first_name, 'Please fill your First Name'];
+          return value;
         }
-      }));
-    }else{
-
-
-      if( this.hearoUser().first_name=='' ){
-        this.warnings.update(value=>{ value.first_name="Please fill your First Name"; return value });
-        setTimeout(()=>{
-          this.warnings.update(value=>{ value.first_name=""; return value });
-        }, env.TIME_ERROR_DISPLAY);
-      }
-      if( this.hearoUser().last_name=='' ){
-        this.warnings.update(value=>{ value.last_name="Please fill your Last Name"; return value });
-        setTimeout(()=>{
-          this.warnings.update(value=>{ value.last_name=""; return value });
-        }, env.TIME_ERROR_DISPLAY);
-      }
-      if( this.hearoUser().email=='' ){
-        this.warnings.update(value=>{ value.email="Please fill your Email"; return value });
-        setTimeout(()=>{
-          this.warnings.update(value=>{ value.email=""; return value });
-        }, env.TIME_ERROR_DISPLAY);
-      }
-      if( this.hearoUser().username=='' ){
-        this.warnings.update(value=>{ value.username="Please fill your desired username"; return value });
-        setTimeout(()=>{
-          this.warnings.update(value=>{ value.username=""; return value });
-        }, env.TIME_ERROR_DISPLAY);
-      }
-      if( this.hearoUser().password=='' ){
-        this.warnings.update(value=>{ value.password="Please create your password"; return value });
-        setTimeout(()=>{
-          this.warnings.update(value=>{ value.password=""; return value });
-        }, env.TIME_ERROR_DISPLAY);
-      }
-      if( this.hearoUser().retype_password!=this.hearoUser().password ){
-        this.warnings.update(value=>{ value.retype_password="Retyped password doesn't match password"; return value });
-        setTimeout(()=>{
-          this.warnings.update(value=>{ value.retype_password=""; return value });
-        }, env.TIME_ERROR_DISPLAY);
-      }
-
-
+      );
+      setTimeout(()=>{
+        this.warnings.update(value=>{ value.first_name=[]; return value;});
+      }, env.TIME_ERROR_DISPLAY);
     }
+    if( this.hearoUser().last_name=='' ){
+      this.warnings.update(
+        value=>{
+          value.last_name= [...value.last_name, 'Please fill your Last Name'];
+          return value;
+        }
+      );
+      setTimeout(()=>{
+        this.warnings.update(value=>{ value.last_name=[]; return value;});
+      }, env.TIME_ERROR_DISPLAY);
+    }
+    if( this.hearoUser().email=='' ){
+      this.warnings.update(
+        value=>{
+          value.email= [...value.email, 'Please fill your Email'];
+          return value;
+        }
+      );
+      setTimeout(()=>{
+        this.warnings.update(value=>{ value.email=[]; return value;});
+      }, env.TIME_ERROR_DISPLAY);
+    }
+    if( this.hearoUser().username=='' ){
+      this.warnings.update(
+        value=>{
+          value.username= [...value.username, 'Please fill your desired username'];
+          return value;
+        }
+      );
+      setTimeout(()=>{
+        this.warnings.update(value=>{ value.username=[]; return value;});
+      }, env.TIME_ERROR_DISPLAY);
+    }
+    if( this.hearoUser().password=='' ){
+      this.warnings.update(
+        value=>{
+          value.password= [...value.password, 'Please create your password'];
+          return value;
+        }
+      );
+      setTimeout(()=>{
+        this.warnings.update(value=>{ value.password=[]; return value;});
+      }, env.TIME_ERROR_DISPLAY);
+    }
+    if( this.hearoUser().retype_password!=this.hearoUser().password ){
+      this.warnings.update(
+        value=>{
+          value.retype_password= [...value.retype_password, "Retyped password doesn't match password"];
+          return value;
+        }
+      );
+      setTimeout(()=>{
+        this.warnings.update(value=>{ value.retype_password=[]; return value;});
+      }, env.TIME_ERROR_DISPLAY);
+    }
+  }
+  private __displayWarningsFromResponse(err:any){
+    if(err.error.user.first_name!=null){
+      for(const warn_first_name of err.error.user.first_name){
+        this.warnings.update(value=>{
+          value.first_name= [...value.first_name, warn_first_name];
+          return value;
+        });
+      }
+      setTimeout(()=>{
+        this.warnings.update(value=>{ value.first_name=[]; return value;});
+      }, env.TIME_ERROR_DISPLAY);
+    }
+    if(err.error.user.last_name!=null){
+      for(const warn_last_name of err.error.user.last_name){
+        this.warnings.update(value=>{
+          value.last_name= [...value.last_name, warn_last_name];
+          return value;
+        });
+      }
+      setTimeout(()=>{
+        this.warnings.update(value=>{ value.last_name=[]; return value;});
+      }, env.TIME_ERROR_DISPLAY);
+    }
+    if(err.error.user.email!=null){
+      for(const warn_email of err.error.user.email){
+        this.warnings.update(value=>{
+          value.email= [...value.email, warn_email];
+          return value;
+        });
+      }
+      setTimeout(()=>{
+        this.warnings.update(value=>{ value.email=[]; return value;});
+      }, env.TIME_ERROR_DISPLAY);
+    }
+    if(err.error.user.username!=null){
+      for(const warn_username of err.error.user.username){
+        this.warnings.update(value=>{
+          value.username= [...value.username, warn_username];
+          return value;
+        });
+      }
+      setTimeout(()=>{
+        this.warnings.update(value=>{ value.username=[]; return value;});
+      }, env.TIME_ERROR_DISPLAY);
+    }
+    if(err.error.user.password!=null){
+      for(const warn_password of err.error.user.password){
+        this.warnings.update(value=>{
+          value.password= [...value.password, warn_password];
+          return value;
+        });
+      }
+      setTimeout(()=>{
+        this.warnings.update(value=>{ value.password=[]; return value;});
+      }, env.TIME_ERROR_DISPLAY);
+    }
+  }
 
+
+  protected async createAccount(): Promise<void>{
+    if( this.__checkIfAllFilled() ){
+      try{
+        await firstValueFrom(this.authUser.createHearoAccountHttpPost(this.hearoUser()));
+        this.warnings.update(value=>{ value.retype_password=['Created Account Successfully ✔']; return value;});
+        setTimeout(()=>{
+          this.authUser.qrAccessAccountRemoveAsync();
+        }, env.TIME_ERROR_DISPLAY);
+      }catch(err: any){
+        this.__displayWarningsFromResponse(err);
+      }
+    }else{
+      this.__writeWarnings2fillUp();
+    }
   }
 }
