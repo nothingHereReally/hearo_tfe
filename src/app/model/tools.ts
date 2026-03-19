9ebb783714e11a8d2788e98e608c246e07efefd4
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-03-19 14:53:05 +0800

    added function for is email format already

--- src/app/model/tools.ts
+++ src/app/model/tools.ts
@@ -16,3 +16,9 @@ export async function sleepAsync(ms: number, callback?: ()=>void): Promise<void>
     resolve();
   }, ms));
 }
+export function isEmailFormat(email: string): boolean{
+  const emailRegex: RegExp= /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if(!email) return false;
+
+  return emailRegex.test(email);
+}
