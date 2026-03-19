7b0e23e63a97ab188b3648288722cc3ef106f0e3
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-03-19 16:54:39 +0800

    finally connected forgot password to api back-end

--- src/app/page/forgot-password/forgot-password.ts
+++ src/app/page/forgot-password/forgot-password.ts
@@ -3,7 +3,9 @@ import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core
 
 import { AuthUser } from '../../api-service/auth-user';
 import { environment as env } from '../../../environment/environment';
-import { isEmailFormat, sleepAsync } from '../../model/tools';
+import { sleepAsync } from '../../model/tools';
+import { ForgotPasswordField, ForgotPasswordResponse } from '../../model/account';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-forgot-password',
@@ -12,7 +14,9 @@ import { isEmailFormat, sleepAsync } from '../../model/tools';
   styleUrl: './forgot-password.css'
 })
 export class ForgotPassword implements OnInit{
-  protected userEmail: WritableSignal<string>= signal('');
+  protected userEmail: WritableSignal<ForgotPasswordField>= signal({
+    email: ''
+  });
   protected warningEmail: WritableSignal<string>= signal('');
   protected successEmail: WritableSignal<string>= signal('');
   private authUser: AuthUser= inject(AuthUser);
@@ -23,18 +27,32 @@ export class ForgotPassword implements OnInit{
   }
 
 
-  protected getResetLinkClicked(): void{
-    if(isEmailFormat(this.userEmail())){
-      this.successEmail.set('Reset Link sent to Email. ✔')
-      sleepAsync(
-        env.TIME_ERROR_DISPLAY,
-        ()=>{
-          this.successEmail.set('');
-          this.authUser.goTo_login_pageIfNotValidAuthTokenAsync();
-        }
-      );
+  private async __requestResetPasswordSend2email(): Promise<void>{
+      try{
+        const reponse: ForgotPasswordResponse= await firstValueFrom(this.authUser.forgotPasswordHttpPost(this.userEmail()));
+        this.successEmail.set(reponse.detail);
+        sleepAsync(
+          env.TIME_ERROR_DISPLAY/3.0,
+          ()=>{
+            this.successEmail.set('');
+            this.authUser.goTo_login_pageIfNotValidAuthTokenAsync();
+          }
+        );
+      }catch(err:any){
+        this.warningEmail.set(err.error.detail)
+        sleepAsync(
+          env.TIME_ERROR_DISPLAY,
+          ()=>{
+            this.warningEmail.set('');
+          }
+        );
+      }
+  }
+  protected async getResetLinkClicked(): Promise<void>{
+    if(this.userEmail().email!=''){
+      await this.__requestResetPasswordSend2email();
     }else{
-      this.warningEmail.set('❌Please provide a correct Email. ❌')
+      this.warningEmail.set('❌Please provide a correct Email. ❌');
       sleepAsync(
         env.TIME_ERROR_DISPLAY,
         ()=>{
