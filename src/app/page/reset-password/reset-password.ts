1345ffd30a4783233b01ede3319553da012e07b7
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-03-19 22:05:56 +0800

    added background for reset password

--- src/app/page/reset-password/reset-password.ts
+++ src/app/page/reset-password/reset-password.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-reset-password',
@@ -6,6 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './reset-password.html',
   styleUrl: './reset-password.css'
 })
-export class ResetPassword {
+export class ResetPassword implements OnInit{
+  private activatedRoute: ActivatedRoute= inject(ActivatedRoute);
+  private resetPasswordPath: string= '/reset-password/';
 
+
+  ngOnInit(): void {
+    this.resetPasswordPath= `${this.resetPasswordPath}${this.activatedRoute.snapshot.params['userb64']}/`;
+    this.resetPasswordPath= `${this.resetPasswordPath}${this.activatedRoute.snapshot.params['usertoken']}/`;
+    console.log(this.resetPasswordPath);
+  }
 }
