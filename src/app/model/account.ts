c2a37d7b2f5cc8dcdde87f4d9ae2ca93a9e15160
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-03-26 22:33:39 +0800

    created HearoTeamGetWithIdResponse for json structure of getting user info

--- src/app/model/account.ts
+++ src/app/model/account.ts
@@ -20,6 +20,22 @@ export type UserEssentialInfo= {
   email: string;
   username: string;
 }
+export type HearoTeamGetWithIdResponse= {
+  email_verified: boolean,
+  is_access_account: boolean,
+  last_update: string,
+  profile_picture: string,
+  user: {
+    date_joined: string,
+    email: string
+    first_name: string,
+    id: number
+    last_login: string,
+    last_name: string,
+    password_last_modified: string,
+    username: string
+  }
+}
 
 
 export type ForgotPasswordField= {
