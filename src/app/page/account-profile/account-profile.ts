7d354708aff6ecc900fa40b54953ca4eef4f239f
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-03-29 11:22:13 +0800

    added structure for /account-profile page

--- src/app/page/account-profile/account-profile.ts
+++ src/app/page/account-profile/account-profile.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
+import { RegisterUser } from '../../model/account';
 
 @Component({
   selector: 'app-account-profile',
@@ -7,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrl: './account-profile.css'
 })
 export class AccountProfile {
+  protected userInfoEdit: WritableSignal<RegisterUser>= signal({
+    first_name: '',
+    last_name: '',
+    email: '',
+    username: '',
+    password: '',
+    retype_password: ''
+  });
 
+
+  /**
+   * clickedBack()
+   * default bo back to /home/sentence
+   * to update to pass past data, else use to /home/sentence
+   */
+  protected clickedBack(): void{
+    console.log(`go back blah ------- ${Math.random()}`);
+  }
+  protected clickedUploadPhoto(): void{
+    console.log(`clicked upload photo ------- ${Math.random()}`);
+  }
+  protected toggleEditDone(): void{
+    console.log(`toggle edit done ------- ${Math.random()}`);
+  }
+  protected toggleAccessAccount(): void{
+    console.log(`toggle access account ------- ${Math.random()}`);
+  }
+  protected clickedDELETEAccount(): void{
+    console.log(`clicked delete account ------- ${Math.random()}`);
+  }
 }
