1b5c267f80e845c93cd8a09a20e1a4e17cfe1815
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-04-02 06:23:06 +0800

    simplified implementation due to be using http interceptor

--- src/app/api-service/api-file.ts
+++ src/app/api-service/api-file.ts
@@ -5,10 +5,8 @@ import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 
 import { AddAuthTokenHttpIntercept } from '../services/auth-token-http-intercept-interceptor';
-import { AuthUser } from './auth-user';
 import { environment as env } from '../../environment/environment';
 import { httpRequestHeadersSendReceiveJson } from '../model/tools';
-import { Token } from '../model/token';
 
 
 @Injectable({
@@ -17,35 +15,33 @@ import { Token } from '../model/token';
 export class ApiFile {
   private http= inject(HttpClient);
   private sanitizer: DomSanitizer= inject(DomSanitizer);
-  private authUser: AuthUser= inject(AuthUser);
 
   private cachedProfilePicture: SafeUrl|null= null;
 
   private async __getProfilePictureViaSafeUrlAsync(): Promise<SafeUrl>{
-    const authToken: Token|null= this.authUser.getAccountToken();
     let errorOut: any= Error("User must be logged in, incorrect implementation.");
-    if( authToken!=null ){
-      try{
-        const imgBlob= await firstValueFrom(this.http.get(
-          `${env.API_DOMAIN}api/v1/get-profile-picture/`,
-          {
-            headers: httpRequestHeadersSendReceiveJson,
-            observe: 'body',
-            responseType: 'blob',
-            context: AddAuthTokenHttpIntercept
-          },
-        ));
-        const objectUrl = URL.createObjectURL(imgBlob);
-        return this.sanitizer.bypassSecurityTrustUrl(objectUrl);
-      }catch (error) {
-        errorOut= error;
-      }
+    try{
+
+      const imgBlob= await firstValueFrom(this.http.get(
+        `${env.API_DOMAIN}api/v1/get-profile-picture/`,
+        {
+          headers: httpRequestHeadersSendReceiveJson,
+          observe: 'body',
+          responseType: 'blob',
+          context: AddAuthTokenHttpIntercept
+        },
+      ));
+      const objectUrl = URL.createObjectURL(imgBlob);
+      return this.sanitizer.bypassSecurityTrustUrl(objectUrl);
+
+    }catch (error) {
+      errorOut= error;
     }
     throw errorOut;
   }
   /**
-  * getProfilePictureViaSafeUrlAsync() to be used only when logged in
-  */
+   * getProfilePictureViaSafeUrlAsync() to be used only when logged in
+   */
   public async getProfilePictureViaSafeUrlAsync(force: boolean=false): Promise<SafeUrl>{
     try{
       if( this.cachedProfilePicture==null || force ){
