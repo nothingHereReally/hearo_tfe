35b84bb9036406191435f790baf7f1e7de3cf9ef
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-04-02 20:29:29 +0800

    added space for readability

--- src/app/model/tools.ts
+++ src/app/model/tools.ts
@@ -8,6 +8,10 @@ export const httpRequestHeadersSendReceiveJson: HttpHeaders= new HttpHeaders({
 export const httpRequestHeadersReceiveJson: HttpHeaders= new HttpHeaders({
    "Accept": "application/json"
 });
+
+
+
+
 export async function sleepAsync(ms: number, callback?: ()=>void): Promise<void>{
   return new Promise(resolve=> setTimeout(()=>{
     if(callback){
@@ -16,18 +20,30 @@ export async function sleepAsync(ms: number, callback?: ()=>void): Promise<void>
     resolve();
   }, ms));
 }
+
+
+
+
 export function isEmailFormat(email: string): boolean{
   const emailRegex: RegExp= /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   if(!email) return false;
 
   return emailRegex.test(email);
 }
+
+
+
+
 export function hasSpace(wordStr: string): boolean{
   const hasSpaceRegExp: RegExp= /\s/;
   if(!wordStr) return false;
 
   return hasSpaceRegExp.test(wordStr);
 }
+
+
+
+
 export function allAreSpace(wordStr: string): boolean{
   const hasSpaceRegExp: RegExp= /^\s+$/;
   if(!wordStr) return false;
