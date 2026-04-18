786097c89fae8bb6b361229a0375f32abdb647a9
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-04-18 14:06:40 +0800

    changed color to be same format on style.css

--- src/app/services/color-service.ts
+++ src/app/services/color-service.ts
@@ -4,35 +4,33 @@ import { Injectable, signal, Signal } from '@angular/core';
   providedIn: 'root',
 })
 export class ColorService {
-  public readonly pc1_100_rgb: Signal<string>= signal('rgba(13, 48, 39, 1.0)');
-  public readonly pc1_200_rgb: Signal<string>= signal('rgba(25, 88, 72, 1.0)');
-  public readonly pc1_300_rgb: Signal<string>= signal('rgba(39, 139, 112, 1.0)');
-  public readonly pc1_400_rgb: Signal<string>= signal('rgba(46, 166, 132, 1.0)');
-  public readonly pc1_500_rgb: Signal<string>= signal('rgba(53, 191, 155, 1.0)');
-  public readonly pc1_600_rgb: Signal<string>= signal('rgba(96, 210, 180, 1.0)');
-  public readonly pc1_700_rgb: Signal<string>= signal('rgba(138, 222, 199, 1.0)');
-  public readonly pc1_800_rgb: Signal<string>= signal('rgba(194, 237, 227, 1.0)');
-  public readonly pc1_900_rgb: Signal<string>= signal('rgba(244, 252, 249, 1.0)');
+  public readonly pc1_100_rgb: Signal<string>= signal('#0D3027');
+  public readonly pc1_200_rgb: Signal<string>= signal('#195848');
+  public readonly pc1_300_rgb: Signal<string>= signal('#278B70');
+  public readonly pc1_400_rgb: Signal<string>= signal('#2EA684');
+  public readonly pc1_500_rgb: Signal<string>= signal('#35BF9B');
+  public readonly pc1_600_rgb: Signal<string>= signal('#60D2B4');
+  public readonly pc1_700_rgb: Signal<string>= signal('#8ADEC7');
+  public readonly pc1_800_rgb: Signal<string>= signal('#C2EDE3');
+  public readonly pc1_900_rgb: Signal<string>= signal('#F4FCF9');
 
+  public readonly pc2_100_rgb: Signal<string>= signal('#11341F');
+  public readonly pc2_200_rgb: Signal<string>= signal('#1C5534');
+  public readonly pc2_300_rgb: Signal<string>= signal('#2A804E');
+  public readonly pc2_400_rgb: Signal<string>= signal('#3AAE6B');
+  public readonly pc2_500_rgb: Signal<string>= signal('#57C785');
+  public readonly pc2_600_rgb: Signal<string>= signal('#81D5A3');
+  public readonly pc2_700_rgb: Signal<string>= signal('#A2E1BA');
+  public readonly pc2_800_rgb: Signal<string>= signal('#BEE8D0');
+  public readonly pc2_900_rgb: Signal<string>= signal('#F4FBF7');
 
-  public readonly pc2_100_rgb: Signal<string>= signal('rgba(17, 52, 31, 1.0)');
-  public readonly pc2_200_rgb: Signal<string>= signal('rgba(28, 85, 52, 1.0)');
-  public readonly pc2_300_rgb: Signal<string>= signal('rgba(42, 128, 78, 1.0)');
-  public readonly pc2_400_rgb: Signal<string>= signal('rgba(58, 174, 107, 1.0)');
-  public readonly pc2_500_rgb: Signal<string>= signal('rgba(87, 199, 133, 1.0)');
-  public readonly pc2_600_rgb: Signal<string>= signal('rgba(129, 213, 163, 1.0)');
-  public readonly pc2_700_rgb: Signal<string>= signal('rgba(162, 225, 186, 1.0)');
-  public readonly pc2_800_rgb: Signal<string>= signal('rgba(190, 232, 208, 1.0)');
-  public readonly pc2_900_rgb: Signal<string>= signal('rgba(244, 251, 247, 1.0)');
-
-
-  public readonly pc3_100_rgb: Signal<string>= signal('rgba(54, 49, 6, 1.0)');
-  public readonly pc3_200_rgb: Signal<string>= signal('rgba(96, 91, 11, 1.0)');
-  public readonly pc3_300_rgb: Signal<string>= signal('rgba(153, 138, 16, 1.0)');
-  public readonly pc3_400_rgb: Signal<string>= signal('rgba(198, 179, 21, 1.0)');
-  public readonly pc3_500_rgb: Signal<string>= signal('rgba(233, 211, 38, 1.0)');
-  public readonly pc3_600_rgb: Signal<string>= signal('rgba(237, 221, 83, 1.0)');
-  public readonly pc3_700_rgb: Signal<string>= signal('rgba(241, 230, 131, 1.0)');
-  public readonly pc3_800_rgb: Signal<string>= signal('rgba(247, 239, 177, 1.0)');
-  public readonly pc3_900_rgb: Signal<string>= signal('rgba(253, 252, 241, 1.0)');
+  public readonly pc3_100_rgb: Signal<string>= signal('#363106');
+  public readonly pc3_200_rgb: Signal<string>= signal('#605B0B');
+  public readonly pc3_300_rgb: Signal<string>= signal('#998A10');
+  public readonly pc3_400_rgb: Signal<string>= signal('#C6B315');
+  public readonly pc3_500_rgb: Signal<string>= signal('#E9D326');
+  public readonly pc3_600_rgb: Signal<string>= signal('#EDDD53');
+  public readonly pc3_700_rgb: Signal<string>= signal('#F1E683');
+  public readonly pc3_800_rgb: Signal<string>= signal('#F7EFB1');
+  public readonly pc3_900_rgb: Signal<string>= signal('#FDFCF1');
 }
