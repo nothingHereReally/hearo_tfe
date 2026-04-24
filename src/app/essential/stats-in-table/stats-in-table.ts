4c8d0f2acc7aea5ad7e3cf2077dc08dd7e1edec7
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-04-24 23:47:55 +0800

    success use InputSignal to pass data from parent to child

--- src/app/essential/stats-in-table/stats-in-table.ts
+++ src/app/essential/stats-in-table/stats-in-table.ts
@@ -1,82 +1,25 @@
-import { Component, OnInit, signal, WritableSignal } from '@angular/core';
+import { Component, input, InputSignal, OnInit } from '@angular/core';
 import { TableColumnAccuracy, TableColumnStatusPatientVideo, TableColumnString, TableColumnURLLink, } from '../../model/table-column';
+import { PieChartPercent } from '../../chart/pie-chart-percent/pie-chart-percent';
+import { Button } from '../button/button';
 
 
 @Component({
   selector: 'app-stats-in-table',
   imports: [
+    Button,
+    PieChartPercent
   ],
   templateUrl: './stats-in-table.html',
   styleUrl: './stats-in-table.css',
 })
 export class StatsInTable implements OnInit{
-  dataSource: WritableSignal<Array<TableColumnString | TableColumnAccuracy | TableColumnStatusPatientVideo | TableColumnURLLink>>= signal([
-    {
-      name: 'string column',
-      data: [
-        'row 1',
-        '2nd row',
-        'row 3',
-        '4th row',
-        'row 5',
-      ],
-      type: 'string'
-    },
-    {
-      name: 'accuracy high column',
-      accuracyType: 'goodHigh',
-      data: [0.91, 0.84, 0.75, 0.60, 0.55],
-      type: 'accuracy'
-    },
-    {
-      name: 'accuracy low column',
-      accuracyType: 'goodLow',
-      data: [0.53, 0.62, 0.8, 0.82, 0.84],
-      type: 'accuracy'
-    },
-    {
-      name: 'status patient video',
-      data: [
-        "In Progress",
-        "Pending",
-        "Done",
-        "Pending",
-        "In Progress"
-      ],
-      type: 'statusPatientVideo'
-    },
-    {
-      name: 'url link column',
-      data: [
-        {
-          link: '/account-profile',
-          label: 'Go fuck ur self',
-          colorButton: 'red'
-        },
-        {
-          link: '/account-profile',
-          label: 'Fuck Off',
-          colorButton: 'blue'
-        },
-        {
-          link: '/account-profile',
-          label: 'Fuck this stupid life',
-          colorButton: 'blue'
-        },
-        {
-          link: '/account-profile',
-          label: 'fuck life',
-          colorButton: 'red'
-        },
-        {
-          link: '/account-profile',
-          label: 'fuck this',
-          colorButton: 'blue'
-        },
-      ],
-      type: 'urlLink'
-    }
-  ]);
+  public dataOnTable: InputSignal<Array<
+      TableColumnString |
+      TableColumnAccuracy |
+      TableColumnStatusPatientVideo |
+      TableColumnURLLink
+  >>= input.required();
 
 
 
