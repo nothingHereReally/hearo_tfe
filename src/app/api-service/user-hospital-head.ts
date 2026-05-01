f703f9474bf72ccd6974b3802dca8bcadc6e2870
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-05-02 00:39:15 +0800

    added read capability on API

--- src/app/api-service/user-hospital-head.ts
+++ src/app/api-service/user-hospital-head.ts
@@ -5,7 +5,8 @@ import { inject, Injectable } from '@angular/core';
 import { environment as env } from '../../environment/environment';
 import { httpRequestHeadersSendReceiveJson } from '../model/tools';
 import { AddAuthTokenHttpIntercept } from '../services/auth-token-http-intercept-interceptor';
-import { Observable } from 'rxjs';
+import { firstValueFrom, Observable } from 'rxjs';
+import { ResponseHospitalHead, RowResponseHospitalHead } from '../model/hospital-head';
 
 
 @Injectable({
@@ -15,15 +16,64 @@ export class UserHospitalHead{
   private http: HttpClient= inject(HttpClient);
 
 
-  public getHospitalHead(): Observable<any>{
-    return this.http.get<any>(
-      `${env.API_DOMAIN}/api/v1/hospital-heads/`,
+  private readHospitalHeads: RowResponseHospitalHead|null= null;
+  private readonly initHospitalHeadUrl: string= `${env.API_DOMAIN}api/v1/hospital-heads/`;
+  private hospitalHeadUrl: string= `${this.initHospitalHeadUrl}`;
+
+
+  private getHospitalHeadHttpGet(): Observable<RowResponseHospitalHead>{
+    return this.http.get<RowResponseHospitalHead>(
+      this.hospitalHeadUrl,
       {
         headers: httpRequestHeadersSendReceiveJson,
-      observe: 'body',
-      credentials: 'include',
-      context: AddAuthTokenHttpIntercept
+        observe: 'body',
+        credentials: 'include',
+        context: AddAuthTokenHttpIntercept
       }
     );
+  }
+
+
+  public async updateListOfHospitalHeads(): Promise<void>{
+    this.hospitalHeadUrl= this.initHospitalHeadUrl;
+    this.readHospitalHeads= await firstValueFrom(this.getHospitalHeadHttpGet());
+  }
+  public async goPrevHospitalHeads(): Promise<void>{
+    if( this.readHospitalHeads?.previous ){
+      this.hospitalHeadUrl= this.readHospitalHeads.previous;
+      this.readHospitalHeads= await firstValueFrom(this.getHospitalHeadHttpGet());
+    }
+  }
+  public async goNextHospitalHeads(): Promise<void>{
+    if( this.readHospitalHeads?.next ){
+      this.hospitalHeadUrl= this.readHospitalHeads.next;
+      this.readHospitalHeads= await firstValueFrom(this.getHospitalHeadHttpGet());
+    }
+  }
+  public async getHospitalHeads(): Promise<ResponseHospitalHead>{
+    if( ! this.readHospitalHeads ){
+      await this.updateListOfHospitalHeads();
+    }
+    let outResponse: ResponseHospitalHead= {
+      count: this.readHospitalHeads!.count,
+      previous: this.readHospitalHeads?.previous!=null,
+      next: this.readHospitalHeads?.next!=null,
+      results: []
+    };
+    if( 0<this.readHospitalHeads!.count ){
+      outResponse.results= this.readHospitalHeads!.results.map(el=>({
+        ...el,
+        last_update: new Date(el.last_update),
+        user: {
+          ...el.user,
+          password_last_modified: new Date(el.user.password_last_modified),
+          date_joined: new Date(el.user.date_joined),
+          last_login: new Date(el.user.last_login)
+        }
+      }));
+    }
+
+
+    return outResponse;
   }
 }
