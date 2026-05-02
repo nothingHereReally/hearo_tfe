faaf23f27a0552bbf050806af1a6beaeb9c6a42c
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-05-02 13:07:37 +0800

    added contant for datetime format

--- src/app/model/tools.ts
+++ src/app/model/tools.ts
@@ -8,6 +8,16 @@ export const httpRequestHeadersSendReceiveJson: HttpHeaders= new HttpHeaders({
 export const httpRequestHeadersReceiveJson: HttpHeaders= new HttpHeaders({
    "Accept": "application/json"
 });
+export const dateTimeFormatOption: Intl.DateTimeFormatOptions = {
+    weekday: 'long',            /* Full day name, e.g. "Monday" */
+    year: 'numeric',            /* Year as numeric */
+    month: 'long',              /* Full month name, e.g. "July" */
+    day: 'numeric',             /* Day of the month */
+    hour: 'numeric',            /* Hour (12-hour format is default in some locales) */
+    minute: 'numeric',          /* Minute */
+    hour12: true,               /* AM/PM */
+    timeZone: 'Asia/Singapore', /* for UTC+8 philippine time */
+};
 
 
 
