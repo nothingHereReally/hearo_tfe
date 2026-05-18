3aae930d3864f4dcf1c9fb7b2e3d07749a78cf9d
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-05-19 06:09:53 +0800

    added header for hospital-head-instance

--- src/app/page/hospital-head-instance/hospital-head-instance.ts
+++ src/app/page/hospital-head-instance/hospital-head-instance.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+
+import { Header } from '../../essential/header/header';
+
 
 @Component({
   selector: 'app-hospital-head-instance',
-  imports: [],
+  imports: [
+    Header
+  ],
   templateUrl: './hospital-head-instance.html',
   styleUrl: './hospital-head-instance.css',
 })
-export class HospitalHeadInstance {}
+export class HospitalHeadInstance implements OnInit{
+  private activatedRoute: ActivatedRoute= inject(ActivatedRoute);
+
+
+  private userId: number= 0;
+
+
+  ngOnInit(): void {
+    this.userId= Number(this.activatedRoute.snapshot.params['hhid']);
+    console.log(this.userId);
+  }
+}
