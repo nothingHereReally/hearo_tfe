2429abb111d8a877bc112a1f4110a1edb571e0c5
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-05-19 21:27:38 +0800

    added to be able to go back

--- src/app/page/hospital-head-instance/hospital-head-instance.ts
+++ src/app/page/hospital-head-instance/hospital-head-instance.ts
@@ -1,20 +1,23 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 
 import { Header } from '../../essential/header/header';
+import { Button } from '../../essential/button/button';
 
 
 @Component({
   selector: 'app-hospital-head-instance',
   imports: [
-    Header
+    Header,
+    Button
   ],
   templateUrl: './hospital-head-instance.html',
   styleUrl: './hospital-head-instance.css',
 })
 export class HospitalHeadInstance implements OnInit{
   private activatedRoute: ActivatedRoute= inject(ActivatedRoute);
+  private route: Router= inject(Router);
 
 
   private userId: number= 0;
@@ -23,5 +26,10 @@ export class HospitalHeadInstance implements OnInit{
   ngOnInit(): void {
     this.userId= Number(this.activatedRoute.snapshot.params['hhid']);
     console.log(this.userId);
+  }
+
+
+  protected goBack(){
+    this.route.navigate(['/hospital-head']);
   }
 }
