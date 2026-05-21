5239285776df2839fa20e949465fea64f3818396
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-05-21 21:33:31 +0800

    changed to get the hospitalHeadUser being clicked earlier

--- src/app/page/hospital-head-instance/hospital-head-instance.ts
+++ src/app/page/hospital-head-instance/hospital-head-instance.ts
@@ -1,10 +1,12 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 
 import { Header } from '../../essential/header/header';
 import { Button } from '../../essential/button/button';
 import { HospitalHead as HospitalHeadModel } from '../../model/hospital-head';
+import { UserHospitalHead } from '../../api-service/user-hospital-head';
+import { firstValueFrom } from 'rxjs';
 
 
 @Component({
@@ -17,36 +19,25 @@ import { HospitalHead as HospitalHeadModel } from '../../model/hospital-head';
   styleUrl: './hospital-head-instance.css',
 })
 export class HospitalHeadInstance implements OnInit{
+  private userHospitalHeadService: UserHospitalHead= inject(UserHospitalHead);
   private activatedRoute: ActivatedRoute= inject(ActivatedRoute);
   private route: Router= inject(Router);
-  protected hospitalHeadUser: HospitalHeadModel= {
-    user: {
-      id: 0,
-      email: "",
-      username: "",
-      password_last_modified: new Date(),
-      first_name: "",
-      last_name: "",
-      date_joined: new Date(),
-      last_login: new Date()
-    },
-    hospital_facility: 0,
-    hospital_facility_name: "",
-    other_hospital: null,
-    account_approved: false,
-    is_active: true,
-    email_verified: true,
-    profile_picture: null,
-    last_update: new Date()
-  };
+  protected hospitalHeadUser: WritableSignal<HospitalHeadModel|null>= signal(null);
 
 
-  private userId: number= 0;
 
 
-  ngOnInit(): void {
-    this.userId= Number(this.activatedRoute.snapshot.params['hhid']);
-    console.log(this.userId);
+  async ngOnInit(): Promise<void> {
+    try{
+      this.hospitalHeadUser.set(
+        await firstValueFrom(
+          this.userHospitalHeadService.getHospitalHeadById(
+            Number(this.activatedRoute.snapshot.params['hhid'])
+          )
+        )
+      );
+      console.log(this.hospitalHeadUser());
+    }catch(err){}
   }
 
 
