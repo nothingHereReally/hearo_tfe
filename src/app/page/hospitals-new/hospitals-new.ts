9429aec57c208db05675264b612a69e77cee3c7e
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-05-27 18:17:22 +0800

    finally formed structure for add new hospital facility

--- src/app/page/hospitals-new/hospitals-new.ts
+++ src/app/page/hospitals-new/hospitals-new.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
+
+
+import { Header } from '../../essential/header/header';
+import { Button } from '../../essential/button/button';
+import { Router } from '@angular/router';
+import { Input } from '../../essential/input/input';
+import { AddHospitalFacility } from '../../model/hospital-facility';
+
 
 @Component({
   selector: 'app-hospitals-new',
-  imports: [],
+  imports: [
+    Header,
+    Button,
+    Input
+  ],
   templateUrl: './hospitals-new.html',
   styleUrl: './hospitals-new.css',
 })
-export class HospitalsNew {}
+export class HospitalsNew{
+  private route: Router= inject(Router);
+
+
+  protected newHospitalFacility: WritableSignal<AddHospitalFacility>= signal({
+    name: '',
+    street: '',
+    municipality: ''
+  });
+
+
+  protected clickedBack(): void{
+    this.route.navigate(['/hospitals']);
+  }
+  protected clickedAddHospital(): void{
+    console.log(this.newHospitalFacility());
+    console.log(`hello add, ${Math.random()}`);
+  }
+}
