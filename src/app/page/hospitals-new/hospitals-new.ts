8fa11c0b0c4cca45f6310f6eb69b276bb010ac20
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-05-27 20:46:28 +0800

    finally now /hospitals/new page works correctly

--- src/app/page/hospitals-new/hospitals-new.ts
+++ src/app/page/hospitals-new/hospitals-new.ts
@@ -5,7 +5,11 @@ import { Header } from '../../essential/header/header';
 import { Button } from '../../essential/button/button';
 import { Router } from '@angular/router';
 import { Input } from '../../essential/input/input';
-import { AddHospitalFacility } from '../../model/hospital-facility';
+import { AddHospitalFacility, RowHospitalFacility } from '../../model/hospital-facility';
+import { HospitalFacility } from '../../api-service/hospital-facility';
+import { sleepAsync } from '../../model/tools';
+import { environment as env } from '../../../environment/environment';
+import { firstValueFrom } from 'rxjs';
 
 
 @Component({
@@ -22,6 +26,13 @@ export class HospitalsNew{
   private route: Router= inject(Router);
 
 
+  private hospitalFacilityService: HospitalFacility= inject(HospitalFacility);
+  protected addedSuccessMsg: WritableSignal<string>= signal('');
+  protected warnings: WritableSignal<AddHospitalFacility>= signal({
+    name: '',
+    street: '',
+    municipality: ''
+  });
   protected newHospitalFacility: WritableSignal<AddHospitalFacility>= signal({
     name: '',
     street: '',
@@ -32,8 +43,65 @@ export class HospitalsNew{
   protected clickedBack(): void{
     this.route.navigate(['/hospitals']);
   }
-  protected clickedAddHospital(): void{
-    console.log(this.newHospitalFacility());
-    console.log(`hello add, ${Math.random()}`);
+  protected async clickedAddHospital(): Promise<void>{
+    let allNotEmpty: boolean= true;
+    if( this.newHospitalFacility().name=='' ){
+      allNotEmpty= false;
+      this.warnings.update(val=>({
+        ...val,
+        name: "Hospital Name can't be Empty"
+      }));
+      sleepAsync(env.TIME_ERROR_DISPLAY, ()=>{
+        this.warnings.update(val=>({
+          ...val,
+          name: ""
+        }));
+      });
+    }
+    if( this.newHospitalFacility().street=='' ){
+      allNotEmpty= false;
+      this.warnings.update(val=>({
+        ...val,
+        street: "Street can't be Empty"
+      }));
+      sleepAsync(env.TIME_ERROR_DISPLAY, ()=>{
+        this.warnings.update(val=>({
+          ...val,
+          street: ""
+        }));
+      });
+    }
+    if( this.newHospitalFacility().municipality=='' ){
+      allNotEmpty= false;
+      this.warnings.update(val=>({
+        ...val,
+        municipality: "Municipality can't be Empty"
+      }));
+      sleepAsync(env.TIME_ERROR_DISPLAY, ()=>{
+        this.warnings.update(val=>({
+          ...val,
+          municipality: ""
+        }));
+      });
+    }
+
+
+    if( allNotEmpty ){
+      let addedResponse: RowHospitalFacility= await firstValueFrom(this.hospitalFacilityService.addHospitalFacility(this.newHospitalFacility()));
+      if( addedResponse!=null ){
+        this.newHospitalFacility.set({
+          name: '',
+          street: '',
+          municipality: ''
+        });
+        this.warnings.set({
+          name: '',
+          street: '',
+          municipality: ''
+        });
+        this.addedSuccessMsg.set('New Hospital Facility Added. ✔');
+        sleepAsync(env.TIME_ERROR_DISPLAY, ()=>{this.addedSuccessMsg.set('');});
+      }
+    }
   }
 }
