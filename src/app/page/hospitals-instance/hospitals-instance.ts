7063d1b659f9281e5385aa558d1fd00f1969ad0c
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-05-29 23:54:09 +0800

    added initial look and TS part for /hospitals/HOSPITAL_FACILITY_ID page

--- src/app/page/hospitals-instance/hospitals-instance.ts
+++ src/app/page/hospitals-instance/hospitals-instance.ts
@@ -1,9 +1,17 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Header } from '../../essential/header/header';
+import { Button } from '../../essential/button/button';
+import { Input } from '../../essential/input/input';
+import { AddHospitalFacility, HospitalFacility } from '../../model/hospital-facility';
 
 @Component({
   selector: 'app-hospitals-instance',
-  imports: [],
+  imports: [
+    Header,
+    Button,
+    Input
+  ],
   templateUrl: './hospitals-instance.html',
   styleUrl: './hospitals-instance.css',
 })
@@ -11,9 +19,35 @@ export class HospitalsInstance implements OnInit{
   private activatedRoute: ActivatedRoute= inject(ActivatedRoute);
 
 
+  private route: Router= inject(Router);
+  protected editHospitalFacility: WritableSignal<HospitalFacility>= signal({
+    id: 0,
+    name: '',
+    street: '',
+    municipality: '',
+    date_added: new Date(),
+    last_update: new Date()
+  });
+  protected warnings: WritableSignal<AddHospitalFacility>= signal({
+    name: '',
+    street: '',
+    municipality: ''
+  });
+  protected editSuccessMsg: WritableSignal<string>= signal('');
+
+
   async ngOnInit(): Promise<void> {
     try{
       console.log(Number(this.activatedRoute.snapshot.params['hfid']));
     }catch(err){}
+  }
+
+
+  protected clickedBack(): void{
+    this.route.navigate(['/hospitals']);
+  }
+  protected clickedUpdate(): void{
+    console.log(`update -->`);
+    console.log(this.editHospitalFacility());
   }
 }
